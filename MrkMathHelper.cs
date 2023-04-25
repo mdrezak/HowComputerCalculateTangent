5c472e9dab6f6858d2f12da7397c77475424d3e2
Mr K <md.reza.k.rg@gmail.com>
2023-04-26 04:07:06 +0430

    Add project files.

---
+++ MrkMathHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowComputerCalculateTangent
+{
+    public class MrkMathHelper
+    {
+        
+        public static double AngleToRadian(double angle) // @mdrezak
+        => angle * 3.14159265358979323846 / 180;//3.14159265358979323846 is PI (64 bit)
+
+        public static double Sin2(double radian, double precision = 100)
+        {
+            double term = radian;
+            double sum = 0;
+            for (int i = 1; i <= precision; i += 2)
+            {
+                sum += term;
+                term *= (-1) * radian * radian / ((i + 1) * i);
+            }
+            return sum;
+        }
+
+
+        public static double Tan(double radian, double precision = 100) // @mdrezak
+        => Sin(radian,precision) / Cos(radian);
+
+        public static double Sin(double radian, double precision = 100) // @mdrezak
+        {
+            double sinValue = radian;
+            double term = radian;
+            int i = 1;
+            while (i < precision)
+            {
+                term = -term * radian * radian / ((2 * i) * (2 * i + 1));
+                sinValue += term;
+                i++;
+            }
+            return (sinValue);
+        }
+
+        public static double Cos(double radians) // @mdrezak
+        {
+            double cos = 1;
+            double term = 1;
+            int i = 2;
+            while (Math.Abs(term) > 0.000001)
+            {
+                term *= -1 * radians * radians / (i * (i - 1));
+                cos += term;
+                i += 2;
+            }
+            return cos;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using HowComputerCalculateTangent;
+
+while (true)
+{
+    Console.ResetColor();
+    Console.Write("angle (0-360) : ");
+    var angle = int.Parse(Console.ReadLine()!);
+    var radian = MrkMathHelper.AngleToRadian(angle);
+    Console.WriteLine($"radian : {angle} * PI * 180 = {radian}");
+    Console.Write("precision : ");
+    var precision = int.Parse(Console.ReadLine()!);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    var Sin = MrkMathHelper.Sin(radian, precision);
+    var Cos = MrkMathHelper.Cos(radian);
+    var Tan = MrkMathHelper.Tan(radian);
+    Console.WriteLine($"Sin Of radian by precision {Sin} (Calculeted by Teylor Series)");
+    Console.WriteLine($"Cos Of radian {Cos} (Calculeted by Teylor Series)");
+    Console.WriteLine($"Rounded Cos Of radian {Math.Round(Cos)} (Calculeted by Teylor Series & Rounded)");
+    Console.WriteLine($"Tan Of radian {Tan} (Tan = Sin / Cos)");
+    Console.WriteLine($"Rounded Tan Of radian {Sin / Math.Round(Cos)} (Tan = Sin / Round(Cos))");
+    Console.WriteLine();
+}
